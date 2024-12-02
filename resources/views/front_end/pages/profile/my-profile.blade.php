d4a34cc984cf35ad10745ad1770835069dad40f9
karan <kn@int.iqsay.com>
2024-12-02 15:19:57 +0530

    fixed

--- resources/views/front_end/pages/profile/my-profile.blade.php
+++ resources/views/front_end/pages/profile/my-profile.blade.php
@@ -30,8 +30,9 @@
                     <div class="profile-sec">
                         <div class="user-img">
                         <div class="update_img_user">                               
-                                <form id="profile-pic-form" action="https://hockidraft.com/profile/profile/update-pic" method="POST" enctype="multipart/form-data">
-                                    <input type="hidden" name="_token" value="1yMWXSGgBG959PKYUdeT5x8IraLTTzmpm8a88zLW" autocomplete="off">                        <input type="file" id="profile-pic-input" name="profile_picture" style="display: none;" accept="image/*" onchange="document.getElementById('profile-pic-form').submit()">                              
+                        <form id="profile-pic-form" action="{{ route('profile.update-pic') }}" method="POST" enctype="multipart/form-data">
+                        @csrf                     
+                        <input type="file" id="profile-pic-input" name="profile_picture" style="display: none;" accept="image/*" onchange="document.getElementById('profile-pic-form').submit()">                              
                                     <span class="edit_pan" onclick="document.getElementById('profile-pic-input').click()" ><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                                         <path
                                             d="M362.7 19.3L314.3 67.7 444.3 197.7l48.4-48.4c25-25 25-65.5 0-90.5L453.3 19.3c-25-25-65.5-25-90.5 0zm-71 71L58.6 323.5c-10.4 10.4-18 23.3-22.2 37.4L1 481.2C-1.5 489.7 .8 498.8 7 505s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L421.7 220.3 291.7 90.3z" />
